d16be75b0ff934a9602e1c853de1fca026dbf755
tocsika7 <tocsamanatee@gmail.com>
2021-06-17 22:33:29 +0200

    Added pagination for people

--- src/components/People.js
+++ src/components/People.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import Person from "./Person";
 
-const fetchPeople = async () => {
-  const res = await fetch("http://swapi.dev/api/people");
+const fetchPeople = async (page) => {
+  const res = await fetch(`http://swapi.dev/api/people/?page=${page}`);
   return res.json();
 };
 
 const People = () => {
-  const { data, status } = useQuery("people", fetchPeople);
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(["people", page], () => fetchPeople(page), {
+    keepPreviousData: true,
+  });
   console.log(data);
 
   return (
     <div>
       <h2>People</h2>
+      <button
+        onClick={() => {
+          if (data.previous) {
+            setPage((old) => old - 1);
+          }
+        }}
+      >
+        Previous Page
+      </button>
+      <span>{page}</span>
+      <button
+        onClick={() => {
+          if (data.next) {
+            setPage((old) => old + 1);
+          }
+        }}
+      >
+        Next Page
+      </button>
       {status === "loading" && <div>Loading data...</div>}
       {status === "error" && <div>Error fetching data</div>}
       {status === "success" && (
